9e7635ae7fc9c170060c81b169bda79837edfd2e
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-04-18 20:26:56 +0400

    add files in project

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
 
         step("Настраиваем тестируемую страницу", () -> {
             Configuration.baseUrl = "https://demoqa.com";
-            Configuration.browserSize = "3840x2160";
+            Configuration.browserSize = "1920x1080";
             Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
