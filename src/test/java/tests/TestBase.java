1077aa10ddc1d983a36adf7fdfbf43463635dc2a
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-04-18 20:23:43 +0400

    add files in project

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static io.qameta.allure.Allure.step;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        step("Настраиваем тестируемую страницу", () -> {
+            Configuration.baseUrl = "https://demoqa.com";
+            Configuration.browserSize = "3840x2160";
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+        });
+    }
+
+    @AfterEach
+    void addAttachments() {
+        step("Прикрепляем атачи рез-та теста", () -> {
+            Attach.attachAsText("Add message to end of th test", "some_message");
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+            closeWebDriver();
+        });
+    }
+}
